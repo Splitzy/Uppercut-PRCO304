bcc0003b3178ae49330cb079d336bc215ae34ec5
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2020-01-31 13:51:32 +0000

    Added Basic Movement and Camera
    -Added Basic Left and Right movement
    -Added Basic Camera which follows the players and zooms in whenever they get close enough

---
+++ Uppercut/Assets/_Scripts/FGCamera.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FGCamera : MonoBehaviour
+{
+    public Transform[] targets;
+
+    public Vector3 offset;
+
+    public float smoothTime = 0.5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimit = 50f;
+
+    private Vector3 vel;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Length == 0)
+        {
+            return;
+        }
+
+        Move();
+        Zoom();
+
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPos = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref vel, smoothTime);
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimit);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    Bounds GetEncapsulatingBounds(Bounds b, Transform[] t)
+    {
+        for (int i = 0; i < t.Length; i++)
+        {
+            b.Encapsulate(t[i].position);
+        }
+
+        return b;
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        GetEncapsulatingBounds(bounds, targets);
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Length == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        GetEncapsulatingBounds(bounds, targets);
+
+        return bounds.center;
+    }
+}
---
+++ Uppercut/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 2.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, 0.0f) * speed);
+    }
+}
