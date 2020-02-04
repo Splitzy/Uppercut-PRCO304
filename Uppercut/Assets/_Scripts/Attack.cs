ed0356e1b7fa01962f49d066ecf47ea6dc1006e4
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2020-02-04 15:13:43 +0000

    Added an attack script

---
+++ Uppercut/Assets/_Scripts/Attack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject punch;
+    public float attackSpeed = 1f;
+    public float attackCD = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(punch.SelfActive == true)
+        {
+            punch.setActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackCD -= Time.deltaTime;
+
+        if(attackCD <= 0f)
+        {
+            if (Input.KeyCode("E"))
+            {
+                if (punch.SelfActive == false)
+                {
+                    punch.setActive(true);
+                    attackCD = 1f / attackSpeed;
+                }
+            }
+        }
+    }
+}
+
