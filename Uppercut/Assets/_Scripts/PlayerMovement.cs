1bbdeb007f4585451116f25ab02d833dff1c8758
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2020-03-16 15:35:08 +0000

    Movement Rework
    - Reworked movement to use rb.MovePosition rather than Translate
    - Moved KOScript and FadeInAndOut scripts to the "_Scripts" folder

--- Uppercut/Assets/_Scripts/PlayerMovement.cs
+++ Uppercut/Assets/_Scripts/PlayerMovement.cs
@@ -6,36 +6,38 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public string h = "Horizontal_P1";
-    Vector2 move;
     Animator anim;
     public GameObject model, countdown;
     Countdown go;
+    Rigidbody rb;
+    Vector3 input;
 
     private void Start()
     {
         anim = model.GetComponent<Animator>();
         go = countdown.GetComponent<Countdown>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
         if(go.canAttack)
         {
+            float moveHorizontal = Input.GetAxis(h);
+
+            input = new Vector3(moveHorizontal, 0, 0);
+
             if (Input.GetAxis(h) > 0 || Input.GetAxis(h) < 0)
             {
                 anim.SetInteger("Walk", 1);
             }
-
-            if (Input.GetAxis(h) == 0)
+            else if (Input.GetAxis(h) == 0)
             {
                 anim.SetInteger("Walk", 0);
             }
 
-            move.x = Input.GetAxis(h);
+            rb.MovePosition(transform.position + (input * speed * Time.fixedDeltaTime));
 
-            Vector2 m = new Vector2(move.x, 0) * speed * Time.deltaTime;
-
-            transform.Translate(m);
         }
     }
 }
